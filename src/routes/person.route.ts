f1a6fdcd785131b0471ad87b2a25fc5c986f2a0c
Janis <janis.oteps@gmail.com>
2026-01-16 01:50:47 +0200

    update position routes

--- src/routes/person.route.ts
+++ src/routes/person.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { createPersonController, deletePersonController, updatePersonController } from '../controllers/person.controller';
+import { createPersonController, deletePersonController, updatePersonController, updatePersonPositionController, clearPersonPositionController } from '../controllers/person.controller';
 
 const personRouter = Router();
 
 personRouter.post('/', createPersonController);
+personRouter.patch('/:id/position', updatePersonPositionController);
+personRouter.delete('/:id/position', clearPersonPositionController);
 personRouter.put('/:id', updatePersonController);
 personRouter.delete('/:id', deletePersonController);
 
