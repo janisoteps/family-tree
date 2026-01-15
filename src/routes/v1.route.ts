1731116bbf1309188532363550e5f4da8b0a8cdd
Janis <janis.oteps@gmail.com>
2026-01-15 23:34:42 +0200

    add to v1

--- src/routes/v1.route.ts
+++ src/routes/v1.route.ts
@@ -3,6 +3,7 @@ import personRouter from './person.route';
 import nodeRouter from './node.route';
 import unionRouter from './union.route';
 import parentOfRouter from './parentOf.route';
+import graphRouter from './graph.route';
 
 const v1Router = Router();
 
@@ -10,5 +11,6 @@ v1Router.use('/node', nodeRouter);
 v1Router.use('/person', personRouter);
 v1Router.use('/union', unionRouter);
 v1Router.use('/parent_of', parentOfRouter);
+v1Router.use('/graph', graphRouter);
 
 export default v1Router;
